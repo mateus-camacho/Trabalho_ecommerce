86d0a610cc3e24bd5998029f1a21e4df3d7afb8e
teukuka <teukuka.silva@hotmail.com>
2024-11-13 19:49:49 -0300

    conected
    pagina conectado ao mongodb

--- trabalho_nosql/back/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.static('public')); // Servindo arquivos estáticos
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
