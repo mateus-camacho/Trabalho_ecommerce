f28d56b04adbfa7ead961f8279eea69d2e566d8e
Wendel Nogueira <wendelnogueira@unifei.edu.br>
2024-11-28 22:18:58 -0300

    feat: add user/register page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,20 +3,68 @@ import { ArrowLeft } from "lucide-react"
 import { useState } from "react"
 import { Link } from "react-router-dom"
 
+import Login from "./login"
+import Register from './register'
+
 export default function Index() {
     const [page, setPage] = useState<'login' | 'register'>('login')
 
-    return (
-        <div className="w-full min-h-screen bg-blue-500 relative flex-1">
-            <Link to={'/'} className="absolute top-4 left-4">
-                <Button variant={'link'} className="text-white transition-colors duration-300 hover:text-gray-100">
-                    <ArrowLeft width={'20'}></ArrowLeft>
-                </Button>
-            </Link>
+    const handleChangePage = () => {
+        setPage(
+            page === 'login' ? 'register' : 'login'
+        )
+    }
 
-            <aside className="w-full max-w-3xl min-h-screen bg-white ml-auto flex items-center justify-center shadow-sm">
-                <p>a</p>
-            </aside>
+    return (
+        <div className="w-full min-h-screen flex items-center justify-center bg-slate-50">
+            <div className="w-full max-w-xl bg-white rounded-sm shadow-sm border border-slate-100 px-10 py-8 flex flex-col gap-1 relative">
+                <div className="absolute top-4 left-4">
+                    <Link to={'/'}>
+                        <Button variant={'link'}>
+                            <ArrowLeft width={'20'}></ArrowLeft>
+
+                            <span>
+                                voltar a loja
+                            </span>
+                        </Button>
+                    </Link>
+                </div>
+
+                <div className="text-center mt-8 mb-6">
+                    <h1 className="text-gray-600 text-2xl font-bold uppercase">
+                        {
+                            page === 'login' ? 'Login' : 'Register'
+                        }
+                    </h1>
+                    <p className="text-gray-600 font-light">
+                        {
+                            page === 'login' ?
+                                'Utilize seu login e senha para entrar em sua conta!' :
+                                'Utilize o formulário abaixo para criar uma nova conta!'
+                        }
+                    </p>
+                </div>
+
+                {
+                    page === 'login' ?
+                        (
+                            <Login></Login>
+                        ) :
+                        (
+                            <Register></Register>
+                        )
+                }
+
+                <button
+                    className="ml-auto mt-2 text-gray-500 font-bold text-sm transition-colors duration-300 hover:text-blue-500"
+                    onClick={handleChangePage}>
+                    {
+                        page === 'login' ?
+                            'Não possui uma conta?' :
+                            'Já possui uma conta?'
+                    }
+                </button>
+            </div>
         </div>
     )
 }
---
+++ src/pages/login/login.tsx
@@ -0,0 +1,108 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { useState } from "react";
+import { EyeClosedIcon, EyeIcon } from "lucide-react";
+
+
+const loginSchema = z.object({
+    email: z.string()
+        .nonempty('Email é obrigatório!')
+        .email('Email inválido!'),
+    password: z.string()
+        .nonempty('Senha é obrigatória!'),
+});
+
+export default function Login() {
+    const [showPass, setShowPass] = useState(false)
+
+    const loginForm = useForm({
+        resolver: zodResolver(loginSchema),
+    });
+
+    const { handleSubmit, reset } = loginForm;
+
+    const handleLogin = (data: any) => {
+        console.log('data', data)
+        reset();
+    }
+
+    return (
+        <Form {...loginForm}>
+            <form
+                onSubmit={handleSubmit(handleLogin)}
+                className="flex flex-col gap-2 mt-2 w-full max-w-sm mx-auto"
+            >
+                <FormField
+                    control={loginForm.control}
+                    name="email"
+                    defaultValue=""
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="email" type="email" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={loginForm.control}
+                    name="password"
+                    defaultValue=""
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Senha</FormLabel>
+                            <FormControl>
+                                <div className="w-full relative">
+                                    <Input
+                                        placeholder="senha"
+                                        type={
+                                            showPass ? 'text' : 'password'
+                                        }
+                                        className="pr-8"
+                                        {...field} />
+
+                                    <Button
+                                        type="button"
+                                        className="absolute right-0 top-0"
+                                        variant={'link'}
+                                        onClick={() => {
+                                            setShowPass(!showPass)
+                                        }}
+                                    >
+                                        {
+                                            !showPass ? (
+                                                <EyeIcon width={'20'} />
+                                            ) : (
+                                                <EyeClosedIcon width={'20'} />
+                                            )
+                                        }
+                                    </Button>
+                                </div>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <Button className="mt-4 mx-auto w-full max-w-80" type="submit">
+                    entrar
+                </Button>
+            </form>
+        </Form>
+    )
+}
